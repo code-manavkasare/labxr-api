9480ce4d47e2d0f92b1b1906006fa7ce8a7bdefd
Manav Kasare <kmanav0705@gmail.com>
2022-05-27 16:20:18 +0530

    removed pm2 from dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import user from "./routes/user";
 import config from "./services/config";
+import "./services/sync";
 
 const { PORT } = config;
 const app = express();
